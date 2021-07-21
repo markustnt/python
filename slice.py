869d5d6dd2be9c15a5cca547d26b11a805a099b2
Marco Mondello <76594613+markustnt@users.noreply.github.com>
2021-07-21 17:12:50 +0200

    Create slice.py

---
+++ slice.py
@@ -0,0 +1,25 @@
+#markusTNT                *****SLICE*****
+
+word="ciao"
+print(word)
+
+#SLICE
+print(word[2])
+
+word="Ciao a Marco"
+print(word[7:12])
+
+#oppure
+print(word[7:])
+
+#oppure
+print(word[-5:])
+
+#ESERCIZIO
+
+frase = "ciao a tutti"
+alfa1 = frase[:5]
+alfa2 = frase[5]
+alfa3 = frase [6:]
+print(alfa1,alfa2,alfa3)
+print(alfa1+alfa2+alfa3)
