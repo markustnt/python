d4cd2e8b403aa3437c344eca235ba2f395420c20
Marco Mondello <76594613+markustnt@users.noreply.github.com>
2021-07-21 20:37:16 +0200

    Create format.py

---
+++ format.py
@@ -0,0 +1,15 @@
+#markusTNT        *****segnaposto FORMAT*****
+
+
+nome="Elena"
+città="Barcellona"
+print("Io mi chiamo {} e vivo a {}".format(nome,città))
+
+
+#************************************************************************
+
+frase= "Il mio nome è {} , ho {} anni e abito a {}"
+nome="Marculin"
+età = "18"
+luogo="Ciriè"
+print(frase.format(nome,età,luogo))
