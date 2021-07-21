a8007bfa993a5cd6619b2c7ca725d9ed759b357f
Marco Mondello <76594613+markustnt@users.noreply.github.com>
2021-07-21 20:55:21 +0200

    Create stringhe.py

---
+++ stringhe.py
@@ -0,0 +1,20 @@
+#markusTNT
+
+#                           *****Stringhe*****
+
+frase="Hola, mi chiamo Mark"
+
+print(len(frase)) #len farà stampare il numero di caratteri e spazi della stringa
+
+print(frase.upper()) #upper farà stampare la stringa in maiuscolo
+
+print(frase.lower()) #lower farà stampare la stringa in minuscolo
+
+print(frase.capitalize()) #capitalize farà stampare solo la prima lettera in maiuscolo
+
+print(frase.find("Mark")) #find troa delle parole o lettere nella stinga
+
+print(frase.replace("Hola","Ciao")) #replace sostituisce una stringa con un altra all'interno di un'altra stringa
+
+print("Mark" in frase) #in verifica che la stringa sia presente nella variabile
+
