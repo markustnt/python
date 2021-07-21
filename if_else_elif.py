56dfc6e7e0ad5148fe32cdfb48c764baa7b55896
Marco Mondello <76594613+markustnt@users.noreply.github.com>
2021-07-21 21:49:14 +0200

    Create if_else_elif.py

---
+++ if_else_elif.py
@@ -0,0 +1,25 @@
+#markusTNT
+
+#                      *****IF*****
+
+#es1
+nome="Marco"
+nome=nome.lower()
+print(nome)
+
+if nome=="marco":
+    print("Sei il migliore")
+
+print("Fine programma")        
+
+#es2
+num=2
+
+if num==10:
+    print("Numero corretto")
+elif num>10:
+    print("Numero maggiore")
+elif num<10:
+    print("Numero minore")
+else:
+    print("Numero non corretto")
