1422189d5c648ea8d193532324454019d63e8ba6
Marco Mondello <76594613+markustnt@users.noreply.github.com>
2021-07-22 21:55:54 +0200

    Create if_elif_else_2.py

---
+++ if_elif_else_2.py
@@ -0,0 +1,11 @@
+#markusTNT                       *****esercizio*****
+
+num=int(input("Inserisci un numero compreso tra 10 e 50: "))
+
+if num>50:
+    print("Hai inserito un numero maggiore di cinquanta")
+elif num<10:
+    print("Hai inserito un numero minore di dieci")
+else:
+    print("Il numero che hai inserito va bene")
+    
