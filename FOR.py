37c692b4c9eab81083c34f1b9db7cdaa17a3d04d
Marco Mondello <76594613+markustnt@users.noreply.github.com>
2021-07-23 15:14:23 +0200

    Create FOR.py

---
+++ FOR.py
@@ -0,0 +1,21 @@
+# FOR
+
+for provola in range(10):
+    print("ciao")
+    print(provola)
+
+print("""
+
+      """)
+    
+for provola in range(5,8):
+    print("ciao")
+    print(provola)
+
+    print("""
+
+      """) 
+    
+    parola="ciao a tutti"
+for provola in parola:
+    print(provola)
