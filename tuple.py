ca0bb3beda9a0b09c5d7b243fe72dca5e231af2c
Marco Mondello <76594613+markustnt@users.noreply.github.com>
2021-07-30 17:20:24 +0200

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,12 @@
+#markusTNT          *****TUPLE*****
+
+#E' come una lista ma con le parentesi tonde ()
+#La tupla non può essere modificata in alcun modo.
+#Si possono usare solo .index oppure .count
+
+oggetti_raccolti=["legna","pietre", "legna", "pietre", "pietre", "diamante", "arco", 'arco', 'freccia','fiore', 'fiore', 'freccia', 'cibo','cibo','cibo','cibo', 'fiore', 'fiore']
+inventario=('legna' ,'pietre', 'arco')
+
+print(inventario.count('legna'))
+print(oggetti_raccolti)
+print(inventario)
