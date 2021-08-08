061dfc372be36d8eea351198249f3f564fc03241
Marco Mondello <76594613+markustnt@users.noreply.github.com>
2021-08-08 04:28:37 +0200

    Create Quiz_Game.py

---
+++ Quiz_Game.py
@@ -0,0 +1,23 @@
+#markusTNT                *****QUIZ*****
+
+print("Quiz Game")
+
+
+lista=['mercurio','venere','terra','marte','giove','saturno','urano','nettuno','plutone']
+lista1=['mercurio','venere','terra','marte','giove','saturno','urano','nettuno','plutone']
+i=1
+while i==1:
+    if len(lista)==0:
+        i=0
+        break
+    else:
+        parola=input("Pianeti del sistema solare:")
+        if parola in lista:
+            print(parola)
+            lista.remove(parola)
+        elif parola in lista1:
+            print('già inserito')
+        else:
+            print('errato')
+
+print('HAI INDOVINATO TUTTE LE PAROLE!!')
